8489f032618487ea598d4b5bfea731dc16a1c8a4
hafifamudi <hafifcyber@gmail.com>
2021-04-28 14:17:26 +0800

    create HTTP Server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("nodeJS Basic");
+const http = require('http');
+
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+ 
+    response.statusCode = 200;
+    response.end('<h1>Hello From nodeJS!</h1>');
+}
+
+
+const server = http.createServer(requestListener);
+ 
+const port = 5000;
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
